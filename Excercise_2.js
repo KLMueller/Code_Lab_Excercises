f1c2f103b7ef18d074651408c3a97472946fede6
KLMueller <111308701+KLMueller@users.noreply.github.com>
2022-08-15 17:41:17 -0500

    Add files via upload

---
+++ Excercise_2.js
@@ -0,0 +1,7 @@
+const myHeading = document.querySelector("h1");
+myHeading.style.color ="blue";
+
+document.querySelector('button').addEventListener('click', function(){
+document.querySelector('h1').style.color='green';
+}
+)
