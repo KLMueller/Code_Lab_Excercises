bffd4ccfc2d7b2c5b56dec3234ee95df067d1e76
KLMueller <111308701+KLMueller@users.noreply.github.com>
2022-08-25 15:39:59 -0500

    Delete Excercise_2.js

--- Excercise_2.js
+++
@@ -1,7 +0,0 @@
-const myHeading = document.querySelector("h1");
-myHeading.style.color ="blue";
-
-document.querySelector('button').addEventListener('click', function(){
-document.querySelector('h1').style.color='green';
-}
-)
