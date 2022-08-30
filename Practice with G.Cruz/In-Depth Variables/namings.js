500d991644702da4ae83499fdb8ef0ddcbf1ee3d
KLMueller <111308701+KLMueller@users.noreply.github.com>
2022-08-30 11:55:01 -0500

    Coding with German Cruz via YouTube
    Includes code that I typed along with while watching the German Cruz Youtube videos. Content in the folder will include practice with "In-Depth Variables", "JavaScript Scopes" and "JavaScript Hoisting".

---
+++ Practice with G.Cruz/In-Depth Variables/namings.js
@@ -0,0 +1,22 @@
+// Variable naming
+// legal
+let TEST;
+let test;
+let woman3;
+let blackDog;
+
+// illegal
+let 1girl;
+    //cannot inlcude numbers first
+let girl%$#@$#%%^;
+    // cannot include special characters
+let;
+    // cannot leave empty
+var let, const, function, while,
+    // cannot use reserved keywords as variable names
+let dfx ={
+    name: "James",
+    age: 10
+};
+    // readability, doesn't represent what we're describing
+    // let somePerson would make more sense there
